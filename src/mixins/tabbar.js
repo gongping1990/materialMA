0931cc4fde3ce695d673a415293868fab876273d
hujinhui <394773007@qq.com>
2019-03-14 18:04:26 +0800

    update

--- src/mixins/tabbar.js
+++ src/mixins/tabbar.js
@@ -5,16 +5,16 @@ export default class tabberMixin extends wepy.mixin {
     onChangeTabbar(event) {
       switch (event.detail) {
         case 0:
-          wx.navigateTo({ url: '/pages/index/index' });
+          wx.redirectTo({ url: '/pages/index/index' });
           break;
         case 1:
-          wx.navigateTo({ url: '/pages/task/task' });
+          wx.redirectTo({ url: '/pages/task/task' });
           break;
         case 2:
-          wx.navigateTo({ url: '/pages/ranking/ranking' });
+          wx.redirectTo({ url: '/pages/ranking/ranking' });
           break;
         case 3:
-          wx.navigateTo({ url: '/pages/user/user' });
+          wx.redirectTo({ url: '/pages/user/user' });
           break;
       }
     }
